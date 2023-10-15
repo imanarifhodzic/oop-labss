09004d127a1ce95b5cca640df553747f0dbe096f
Iman Arifhodzic <iman@Imans-MacBook-Pro.local>
2023-10-15 23:16:54 +0200

    tasks

---
+++ src/main/java/org/example/exercise8part2.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class exercise8part2 {
+    private static void drawStarsPyramidUpsideDown(int rows){
+
+        for (int i=rows; i>=1; i--){
+            for (int j=i; j>=1; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        drawStarsPyramidUpsideDown(5);
+    }
+}
